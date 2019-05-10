7a74c5d8ce4b7cc30f23e10e4849ff3763c531e7
yomij <syomijp@gmail.com>
2019-05-10 22:05:51 +0800

    失败章节修复

--- src/test/reverseTest.js
+++ src/test/reverseTest.js
@@ -185,7 +185,7 @@ async function insertBooksHottest (page = 1, chapters = false) {
 
 async function chapter(s) {
 	const arr = 'https://read.ixdzs.com/66/66746/p3.html'.split('/')
-console.log(	arr.pop().match(/\d+/g)[0], arr.pop())
+	console.log(	arr.pop().match(/\d+/g)[0], arr.pop())
 	// return await reserveIndex.getChapter(s)
 }
 
@@ -194,7 +194,7 @@ console.log(	arr.pop().match(/\d+/g)[0], arr.pop())
 // /d/217/217500/
 
 
-spider('/d/64/64960/')
+// spider('/d/64/64960/')
 // insertBooksHottest(4, true)
 // chapter()
 
@@ -210,3 +210,121 @@ spider('/d/64/64960/')
 // }
 //
 // getChapter()
+
+function dosUpdate(list, index, bookId, subCallback) {
+	async.mapLimit(list, spiderConfig.CONCURRENCY_COUNT, function (cs, callback) {
+		cs = Object.assign(cs, {
+			bookId,
+			_id: bookId + cs.chapterNum
+		})
+
+		const random = Math.random()
+
+		setTimeout(() => {
+			if (random > 0.5) {
+				reserve.getChapter(cs.source).then(res => {
+					console.log(cs.source)
+					cs.content = res
+					cs.success = true
+					callback(null, cs)
+				}).catch(e => {
+					cs.success = false
+					cs.content = ''
+					callback(null, cs)
+				})
+			} else {
+				const id = cs.source.split('/')[4]
+				reserve.getChapterM(id, cs.chapterNum + 1).then(res => {
+					cs.content = res
+					cs.success = true
+					callback(null, cs)
+				}).catch(e => {
+					cs.success = false
+					cs.content = ''
+					callback(null, cs)
+				})
+			}
+
+		}, (cs.chapterNum - spiderConfig.MAX_SINGLE_COUNT * index) * random * spiderConfig.SPIDER_TIMEOUT)
+
+	}, function (err, result) {
+		let failed = []
+		const success = result.filter(item => {
+			if (!item.success) failed.push(item)
+			else chapterDao.updateChapter(item._id, item.content)
+			return item.success
+		})
+		// 写入数据库
+		// chapterDao.insertChapters(result)
+		if (!err) {
+			subCallback(null, {
+				index,
+				success: true,
+				data: {
+					failed,
+					success
+				}
+			})
+		} else {
+			subCallback(null, {
+				index,
+				success: false,
+				data: {
+					failed: list,
+					success: []
+				}
+			})
+		}
+	});
+}
+
+module.exports = {
+	/**
+	 *
+	 * @param bookList {
+	 *   bookId,
+	 *   chapterUrl
+	 * }
+	 * @returns {Promise<void>}
+	 */
+	async  insertFailedBook (bookList) {
+
+		async.mapLimit(bookList, 1, async function (book, callback) {
+			try {
+				const cs = await reserve.getChapters(book.chapterUrl, book.bookId)
+				if (!cs.length) {
+					return Object.assign(book, {message: '章节抓取失败，无章节'})
+				}
+				await chapterDao.insertChapters(cs)
+				let list = []
+				while (cs.length) {
+					list.push(cs.splice(0, spiderConfig.MAX_SINGLE_COUNT))
+				}
+				let index = 0
+				console.log(book.title ? book.title : book + '开始获取')
+				return async.mapLimit(list, 1, function (subList, callback) { // 开始分批抓取章节
+					dosUpdate(subList, index++, book.bookId, callback)
+				}, function (err, result) {
+					const failed = []
+					const success = []
+					result.forEach(item => { // 一本书抓取完成 打印失败内容
+						failed.push(...item.data.failed)
+					})
+					console.log(err,failed.length, JSON.stringify(failed))
+					file.writeJson(failed, book.title + ' ' + book.bookId || 'failed'  + ' ' + book.bookId, 'F_PATH')
+				})
+			} catch (e) {
+				console.log('failed', e.message)
+				// return spider(book)
+				return {
+					book,
+					data: {title: e.message}
+				}
+			}
+
+		}, function (err, result) {
+			if(err) console.log(err.message)
+			file.writeJson(result, 'failedBookReget' + new Date().getTime(), 'P_PATH')
+		})
+	}
+}
